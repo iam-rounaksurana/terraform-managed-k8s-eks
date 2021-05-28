e9bca104119a8429f53e249708b0495c7b3f3bb0
Ubuntu <ubuntu@ip-172-31-44-99.ap-south-1.compute.internal>
2021-05-28 09:36:37 +0000

    Updates-0.2

--- eks-workers.tf
+++ eks-workers.tf
@@ -8,11 +8,6 @@ data "aws_ami" "eks-worker" {
   owners      = ["amazon"] # Amazon
 }
 
-# EKS currently documents this required userdata for EKS worker nodes to
-# properly configure Kubernetes applications on the EC2 instance.
-# We utilize a Terraform local here to simplify Base64 encoding this
-# information into the AutoScaling Launch Configuration.
-# More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
 locals {
   k8s-node-userdata = <<USERDATA
 #!/bin/bash
@@ -42,14 +37,6 @@ resource "aws_autoscaling_group" "k8s" {
   max_size = 2
   min_size = 1
   name = "terraform-eks-k8s"
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   vpc_zone_identifier = module.vpc.public_subnets
 
   tag {
